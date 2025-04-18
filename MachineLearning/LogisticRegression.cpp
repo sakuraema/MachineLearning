8f3d991d45d8fb44356933aa8b8cea2df2709af0
TonyChu <tonychu122003@gmail.com>
2025-04-18 14:48:31 +0800

    Improve implementation

--- MachineLearning/LogisticRegression.cpp
+++ MachineLearning/LogisticRegression.cpp
@@ -1,61 +1,44 @@
-#include "pch.h"
+﻿#include "pch.h"
 #include "LogisticRegression.h"
 
-LogisticRegression::LogisticRegression(double lr, int n_iter)
-    : m_dLearningRate(lr), m_iMaxIterations(n_iter)
+LogisticRegression::LogisticRegression(double dLearningRate, int iMaxIterations)
+    : m_dBias(0), m_dLearningRate(dLearningRate), m_iMaxIterations(iMaxIterations), m_nFeatures(0)
 {
 }
 
-// Train the model using gradient descent
 void LogisticRegression::Train(const std::vector<std::vector<double>>& X, const std::vector<int>& y)
 {
-    // Add bias term and initialize weights
-    auto X_bias = AddBias(X);
-    m_dWeights = std::vector<double>(X_bias[0].size(), 0.0);
+    m_nFeatures = X[0].size();
+    m_dWeights.assign(m_nFeatures, 0.0);
 
-    // Gradient descent
+    std::vector<double> dDeltaWeights(m_nFeatures, 0.0);
+    double dDeltaBias = 0.0;
+
     for (int iter = 0; iter < m_iMaxIterations; ++iter)
     {
-        std::vector<double> gradients(m_dWeights.size(), 0.0);
+        ComputeGradients(X, y, dDeltaWeights, dDeltaBias);
 
-        // Calculate gradients
-        for (size_t i = 0; i < X_bias.size(); ++i)
-        {
-            double predicted = PredictProbability(X[i]);
-            double error = predicted - y[i];
-
-            for (size_t j = 0; j < m_dWeights.size(); ++j)
-            {
-                gradients[j] += error * X_bias[i][j];
-            }
-        }
-
-        // Update weights
-        for (size_t j = 0; j < m_dWeights.size(); ++j)
-        {
-            m_dWeights[j] -= m_dLearningRate * gradients[j] / X.size();
-        }
+        // Update weights and bias
+        for (int j = 0; j < m_nFeatures; ++j)
+            m_dWeights[j] -= m_dLearningRate * dDeltaWeights[j];
+        m_dBias -= m_dLearningRate * dDeltaBias;
     }
 }
 
-// Predict probability for single sample
 double LogisticRegression::PredictProbability(const std::vector<double>& x) const
 {
     double z = m_dWeights[0];  // Bias term
     for (size_t i = 0; i < x.size(); ++i)
-    {
         z += m_dWeights[i + 1] * x[i];
-    }
+
     return ComputeSigmoid(z);
 }
 
-// Predict class label (0 or 1)
 int LogisticRegression::Predict(const std::vector<double>& x) const
 {
     return PredictProbability(x) >= 0.5 ? 1 : 0;
 }
 
-// Sigmoid activation function
 double LogisticRegression::ComputeSigmoid(double z) const
 {
     return 1.0 / (1.0 + exp(-z));
@@ -64,29 +47,35 @@ double LogisticRegression::ComputeSigmoid(double z) const
 std::vector<double> LogisticRegression::ComputeSoftmax(const std::vector<double>& z) const
 {
 	std::vector<double> softmax(z.size());
-	double max_z = *std::max_element(z.begin(), z.end());
-	double sum_exp = 0.0;
+	double dMaxZ = *std::max_element(z.begin(), z.end());
+	double dSumExp = 0.0;
 	for (size_t i = 0; i < z.size(); ++i)
 	{
-		softmax[i] = exp(z[i] - max_z);
-		sum_exp += softmax[i];
+		softmax[i] = exp(z[i] - dMaxZ);
+		dSumExp += softmax[i];
 	}
 	for (size_t i = 0; i < z.size(); ++i)
-	{
-		softmax[i] /= sum_exp;
-	}
+		softmax[i] /= dSumExp;
+
 	return softmax;
 }
 
-// Add bias term (column of 1s) to feature matrix
-std::vector<std::vector<double>> LogisticRegression::AddBias(const std::vector<std::vector<double>>& X) const
+void LogisticRegression::ComputeGradients(const std::vector<std::vector<double>>& X, const std::vector<int>& y, std::vector<double>& dDeltaWeights, double& dDeltaBias) const
 {
-    std::vector<std::vector<double>> X_bias;
-    for (const auto& row : X)
-    {
-        std::vector<double> new_row = { 1.0 };  // Bias term
-        new_row.insert(new_row.end(), row.begin(), row.end());
-        X_bias.push_back(new_row);
+    const size_t n = X.size();
+
+    for (size_t i = 0; i < n; ++i) {
+        // Modified to use sigmoid activation
+        double dPrediction = ComputeSigmoid(Eigen::Map<const Eigen::VectorXd>(X[i].data(), X[i].size()).dot(Eigen::Map<const Eigen::VectorXd>(m_dWeights.data(), m_dWeights.size())) + m_dBias);
+        double dError = dPrediction - y[i];
+
+        for (int j = 0; j < m_nFeatures; ++j)
+            dDeltaWeights[j] += dError * X[i][j];
+        dDeltaBias += dError;
     }
-    return X_bias;
+
+    const double dInvSampleCount = 1.0 / n;
+    for (int j = 0; j < m_nFeatures; ++j)
+        dDeltaWeights[j] *= dInvSampleCount;
+    dDeltaBias *= dInvSampleCount;
 }
--- MachineLearning/LogisticRegression.h
+++ MachineLearning/LogisticRegression.h
@@ -6,28 +6,23 @@ class LogisticRegression
 {
 public:
     LogisticRegression(double lr = 0.01, int n_iter = 1000);
-
-    // Train the model using gradient descent
     void Train(const std::vector<std::vector<double>>& X, const std::vector<int>& y);
-
-    // Predict probability for single sample
-    double PredictProbability(const std::vector<double>& x) const;
-
-    // Predict class label (0 or 1)
     int Predict(const std::vector<double>& x) const;
+    void Clear() { std::fill(m_dWeights.begin(), m_dWeights.end(), 0); m_dBias = 0.0; }
 
     inline std::vector<double> GetWeights() const { return m_dWeights; }
+    inline double GetBias() const { return m_dBias; }
+
+private:
+    double PredictProbability(const std::vector<double>& x) const;
+    double ComputeSigmoid(double z) const;    
+    std::vector<double> ComputeSoftmax(const std::vector<double>& z) const;
+    void ComputeGradients(const std::vector<std::vector<double>>& X, const std::vector<int>& y, std::vector<double>& dDeltaWeights, double& dDeltaBias) const;
 
 private:
     std::vector<double> m_dWeights;  // Includes bias term as first element
+    double m_dBias;
     double m_dLearningRate;
+    int m_nFeatures;
     int m_iMaxIterations;
-
-    // Sigmoid activation function
-    double ComputeSigmoid(double z) const;
-    
-    std::vector<double> ComputeSoftmax(const std::vector<double>& z) const;
-
-    // Add bias term (column of 1s) to feature matrix
-    std::vector<std::vector<double>> AddBias(const std::vector<std::vector<double>>& X) const;
 };
